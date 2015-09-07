2fc211d6869ef6f28fb86df02817dec18c15cd95
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-09-07 15:42:03 -0400

    Initial Commit

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'faker'
 gem 'pg'
 gem 'rake'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'faker'
 require 'sinatra/activerecord'
 require 'sinatra/reloader'
 
